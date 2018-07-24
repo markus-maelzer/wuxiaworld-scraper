07aef4a90077d488a6f4d8cc865ae4823d9e6605
Markus Maelzer <markus.maelzer@gmail.com>
2018-07-24 12:05:23 +0200

    seems to work

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const puppeteer = require('puppeteer');
+const _ = require('lodash');
 
 const scrapeIt = async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true
   });
   const page = await browser.newPage();
 
   await page.goto('https://www.wuxiaworld.com/novel/emperors-domination/emperor-chapter-1');
-  await page.waitFor(100000);
+  // await page.waitFor(1000);
   // click .pg-modal-close;
+  await page.click('.pg-modal-close');
+  // await page.waitFor(1000);
   const pageText = await page.evaluate(() => {
-    const paragraphs = document.querySelectorAll('.fr-view > p');
-    // const text = paragraphs.map(p => {
-    //   return p.innerHTML;
-    // });
-    console.log(paragraphs);
-    return 'text';
+    const paragraphs = document.querySelectorAll('.fr-view p');
+    let text = [];
+    for (var i = 0; i < paragraphs.length; i++) {
+      text.push(paragraphs[i].innerText);
+    }
+    return text;
   })
+  // await page.waitFor(1000);
   console.log(pageText);
   await browser.close();
 }
